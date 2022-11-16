d1dd24a0f8ff13754208d26e514a9927f0ae53b3
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2022-11-17 00:03:13 +0800

    Trainer base classes

---
+++ ABrain/trainer/base.py
@@ -0,0 +1,11 @@
+class Trainer(object):
+    '''定义训练器的基础属性和行为'''
+    pass
+
+class GeneralTrainer(Trainer):
+    '''通用基础训练器'''
+    pass 
+
+class CascadedTrainer(GeneralTrainer):
+    '''通用级联网络训练器'''
+    pass
