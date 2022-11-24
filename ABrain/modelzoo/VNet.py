e2cec660392e137b9d8c94857b0219e0196ba822
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2022-11-24 22:29:20 +0800

    modelzoo: VNet

---
+++ ABrain/modelzoo/VNet.py
@@ -0,0 +1,167 @@
+from typing import List
+import torch
+import torch.nn as nn
+
+from .base import SegmentationModel
+
+
+class VInput(nn.Module):
+    def __init__(self, in_chs: int, out_chs: int) -> None:
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv3d(in_chs, out_chs//2, kernel_size=5, stride=1, padding=2),
+            nn.PReLU()
+        )
+        self.pooling = nn.Conv3d(out_chs//2, out_chs, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = self.conv(x)+x
+        x_sampling = self.pooling(x)
+        return x, x_sampling
+
+
+class VOutput(nn.Module):
+    def __init__(self, in_chs: int, out_chs: int) -> None:
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.ConvTranspose3d(in_chs, in_chs, 5, 1, 2),
+            nn.PReLU()
+        )
+        self.segmenatation = nn.Sequential(
+            nn.Conv3d(in_chs, out_chs, 1, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x1, x2):
+        x = torch.cat((x1, x2), dim=-4)
+        x = self.conv(x)+x
+        x = self.segmenatation(x)
+        return x
+
+
+class VStageDown(nn.Module):
+    def __init__(self, in_chs: int, out_chs: int, n_convs: int) -> None:
+        super().__init__()
+        self.rediualBlock = nn.Sequential()
+        for _ in range(n_convs):
+            self.rediualBlock.append(
+                nn.Conv3d(in_chs, in_chs, 5, 1, 2))
+            self.rediualBlock.append(nn.PReLU())
+        self.sample = nn.Conv3d(in_chs, out_chs, 2, 2)
+
+    def forward(self, x):
+        x = torch.add(x, self.rediualBlock(x))
+        x_sampled = self.sample(x)
+        return x, x_sampled
+
+
+class VStageUp(nn.Module):
+    def __init__(self, in_chs: int, out_chs: int, n_convs: int) -> None:
+        super().__init__()
+        self.rediualBlock = nn.Sequential()
+        for _ in range(n_convs):
+            self.rediualBlock.append(
+                nn.Conv3d(in_chs, in_chs, 5, 1, 2))
+            self.rediualBlock.append(nn.PReLU())
+        self.sample = nn.ConvTranspose3d(in_chs, out_chs//2, 2, 2)
+
+    def forward(self, x1, x2):
+        x = torch.cat((x1, x2), dim=-4)
+        x = torch.add(x, self.rediualBlock(x))
+        x = self.sample(x)
+        return x
+
+
+class VEncoder(nn.Module):
+    def __init__(self, in_chs: int, block_size=List[int], max_chs: int = 256) -> None:
+        super().__init__()
+        self.stages = nn.Sequential()
+        self.out_chs = []
+        for sz in block_size:
+            out = min(max_chs, in_chs*2)
+            block = VStageDown(in_chs, out, sz)
+            self.stages.append(block)
+            in_chs = out
+            self.out_chs.append(out)
+
+    def forward(self, x):
+        skips = []
+        for stage in self.stages:
+            sk, x = stage(x)
+            skips.append(sk)
+        skips.reverse()
+        return skips, x
+
+
+class VDecoder(nn.Module):
+    def __init__(self, in_chs: int, block_size=List[int], min_chs: int = 16) -> None:
+        super().__init__()
+        self.stages = nn.Sequential()
+        block_size: list = block_size.copy()
+        block_size.reverse()
+        for sz in block_size:
+            out_chs = max(min_chs, in_chs//2)
+            block = VStageUp(in_chs, out_chs, sz)
+            self.stages.append(block)
+            in_chs = out_chs
+        self.out_chs = in_chs
+
+    def forward(self, x_stages, x):
+        for stage, xi in zip(self.stages, x_stages):
+            x = stage(xi, x)
+        return x
+
+
+class VBottleneck(nn.Module):
+    def __init__(self, in_chs: int, out_chs: int, n_convs: int,) -> None:
+        super().__init__()
+        self.conv = nn.Sequential()
+        for _ in range(n_convs):
+            self.conv.append(nn.Conv3d(in_chs, in_chs, 5, 1, 2))
+            self.conv.append(nn.PReLU())
+        self.upsample = nn.Sequential(
+            nn.ConvTranspose3d(in_chs, out_chs, 2, 2),
+            nn.PReLU()
+        )
+
+    def forward(self, x):
+        x = self.conv(x)+x
+        x = self.upsample(x)
+        return x
+
+
+class VNetFrameWork(SegmentationModel):
+    def __init__(self, n_class: int,
+                 input_layer: nn.Module, encoder: nn.Module, decoder: nn.Module, bottleneck: nn.Module, output_layer: nn.Module
+                 ) -> None:
+        super().__init__(n_class)
+        self.input = input_layer
+        self.encoder = encoder
+        self.bottleneck = bottleneck
+        self.decoder = decoder
+        self.output = output_layer
+
+    def forward(self, x):
+        x_ori, x = self.input(x)
+        x_stages, x = self.encoder(x)
+        x_bot = self.bottleneck(x)
+        x = self.decoder(x_stages, x_bot)
+        x = self.output(x_ori, x)
+        return x
+
+
+class VNet(VNetFrameWork):
+    def __init__(self, n_class: int, ori_chs: int, in_chs: int, block_size: List[int], max_chs: int = 256, min_chs=16) -> None:
+        input_layer = VInput(ori_chs, in_chs)
+        encoder = VEncoder(in_chs, block_size[:-1], max_chs)
+        bottleneck = VBottleneck(encoder.out_chs,encoder.out_chs//2,block_size[-1])
+        decoder = VDecoder(encoder.out_chs, block_size[:-1], min_chs)
+        output_layer = VOutput(decoder.out_chs,)
+        super().__init__(
+            n_class=n_class,
+            input_layer=input_layer,
+            encoder=encoder,
+            bottleneck=bottleneck,
+            decoder=decoder,
+            output_layer=output_layer
+        )
---
+++ ABrain/tests/models/vnet.py
@@ -0,0 +1,106 @@
+import unittest
+
+import torch
+
+from ...modelzoo.VNet import VStageDown, VStageUp, VBottleneck, VEncoder, VDecoder, VInput, VOutput, VNetFrameWork
+
+
+class TestVNet(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_VStageUp(self):
+        B, C_in, W, H, D = 2, 16, 128, 128, 64
+        C_out = C_in*2
+        S = 3
+        stage: VStageDown = VStageDown(C_in, C_out, S)
+        n = len(stage.rediualBlock)
+        # print(stage.rediualBlock)
+        self.assertEqual(n, S*2)  # a conv with a pooling.
+        x = torch.rand(B, C_in, W, H, D)
+        y, y_small = stage(x)
+        self.assertTrue(True)
+        self.assertEqual(y.shape, (B, C_in, W, H, D))
+        self.assertEqual(y_small.shape, (B, C_out, W//2, H//2, D//2))
+
+    def test_VStageDown(self):
+        B, C_in, W, H, D = 2, 256, 16, 16, 8
+        C_out = C_in//4
+        S = 3
+        stage: VStageUp = VStageUp(C_in, C_in//2, S)
+        n = len(stage.rediualBlock)
+        # print(stage.rediualBlock)
+        self.assertEqual(n, S*2)  # a conv with a pooling.
+        x1 = torch.rand(B, C_in//2, W, H, D)
+        x2 = torch.rand(B, C_in//2, W, H, D)
+        y = stage(x1, x2)
+        self.assertTrue(True)
+        self.assertEqual(y.shape, (B, C_out, W*2, H*2, D*2))
+
+    def test_VIO(self):
+        B, C_in, W, H, D = 2, 1, 128, 128, 64
+        C_out = 32
+        input_layer = VInput(C_in, C_out)
+        # print(input_layer)
+        x = torch.rand(B, C_in, W, H, D)
+        y, y_small = input_layer(x)
+        self.assertEqual(y.shape, (B, C_out//2, W, H, D))
+        self.assertEqual(y_small.shape, (B, C_out, W//2, H//2, D//2))
+
+        B, C_in, W, H, D = 2, 32, 128, 128, 64
+        C_out = 7
+        x1 = torch.rand(B, C_in//2, W, H, D)
+        x2 = torch.rand(B, C_in//2, W, H, D)
+        out_layer = VOutput(C_in, C_out)
+        # print(out_layer)
+        y = out_layer(x1, x2)
+        self.assertEqual(y.shape, (B, C_out, W, H, D))
+
+    def test_bottleneck(self):
+        B, C_in, W, H, D = 2, 256, 8, 8, 4
+        C_out = C_in//2
+        S = 3
+        x = torch.rand(B, C_in, W, H, D)
+        bottleneck: VBottleneck = VBottleneck(C_in, C_out, S)
+        # print(bottleneck)
+        y = bottleneck(x)
+        n = len(bottleneck.conv)
+        self.assertEqual(n, S*2)  # a conv with a activate function
+        self.assertEqual(y.shape, (B, C_out, W*2, H*2, D*2))
+
+    def test_VEncoder(self):
+        B, C_in, W, H, D = 2, 32, 64, 64, 32
+        encoder = VEncoder(C_in, [2, 3, 3])
+        x = torch.rand(B, C_in, W, H, D)
+        x_s, x = encoder(x)
+        self.assertEqual(x.shape, (B, C_in*8, W//8, H//8, D//8))
+        self.assertEqual(len(x_s), 3)
+
+    def test_VDecoder(self):
+        B, C_in, W, H, D = 2, 256, 16, 16, 8
+        decoder = VDecoder(C_in, [2, 3, 3])
+        x = torch.rand(B, C_in//2, W, H, D)
+        xs = [
+            torch.rand(B, C_in//2, W, H, D),
+            torch.rand(B, C_in//4, W*2, H*2, D*2),
+            torch.rand(B, C_in//8, W*4, H*4, D*4),
+        ]
+        x = decoder(xs, x)
+        self.assertEqual(x.shape, (B, C_in//16, W*8, H*8, D*8))
+
+    def test_VNet(self):
+        B, C, W, H, D = 2, 1, 128, 128, 64
+        N = 10
+        img = torch.rand(B, C, W, H, D)
+        [2, 3, 3, 3]
+        [1]
+        vnet = VNetFrameWork(
+            N,
+            input_layer=VInput(1, 32),
+            encoder=VEncoder(32, [2, 3, 3]),
+            bottleneck=VBottleneck(256, 128, 3),
+            decoder=VDecoder(256, [2, 3, 3]),
+            output_layer=VOutput(32, 10)
+        )
+        y = vnet(img)
+        self.assertEqual(y.shape, (B, N, W, H, D))
