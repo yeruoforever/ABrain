0efca39f66a72fea2cad52658c96de414f7a42fc
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2022-11-25 16:10:25 +0800

    test for each segmentation mode

--- ABrain/tests/datasets/ibsr.py
+++ ABrain/tests/datasets/ibsr.py
@@ -13,11 +13,12 @@ class TestIBSR(unittest.TestCase):
 
     def test_ibsr18(self):
         B = 2
-        dataset= IBSR18(os.path.join(self.database,"IBSR18"))
-        for x in data.DataLoader(dataset,batch_size=B,shuffle=True):
-            names = x["name"]
-            imgs = x["img"][tio.DATA]
-            segs = x["seg"][tio.DATA]
-            self.assertEqual(len(names),B)
-            self.assertEqual(imgs.shape,(B,1,256,128,256))
-            self.assertEqual(segs.shape,(B,1,256,128,256))
+        for mode_id in range(3):
+            dataset= IBSR18(os.path.join(self.database,"IBSR18"),mode_id)
+            for x in data.DataLoader(dataset,batch_size=B,shuffle=True):
+                names = x["name"]
+                imgs = x["img"][tio.DATA]
+                segs = x["seg"][tio.DATA]
+                self.assertEqual(len(names),B)
+                self.assertEqual(imgs.shape,(B,1,256,128,256))
+                self.assertEqual(segs.shape,(B,1,256,128,256))
