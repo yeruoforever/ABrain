3a7ac94f04a062457153cc079badac6ab021236a
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2022-11-25 16:06:53 +0800

    Dataset: IBSR18

---
+++ ABrain/tests/datasets/ibsr.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+
+import torch.utils.data as data
+import torchio as tio
+
+from ...dataset.IBSR import IBSR18
+
+class TestIBSR(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.database="/dev/shm/tinyset"
+
+    def test_ibsr18(self):
+        B = 2
+        dataset= IBSR18(os.path.join(self.database,"IBSR18"))
+        for x in data.DataLoader(dataset,batch_size=B,shuffle=True):
+            names = x["name"]
+            imgs = x["img"][tio.DATA]
+            segs = x["seg"][tio.DATA]
+            self.assertEqual(len(names),B)
+            self.assertEqual(imgs.shape,(B,1,256,128,256))
+            self.assertEqual(segs.shape,(B,1,256,128,256))
