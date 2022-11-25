b6c0282139343dc67d3156a35c2a63cc824d94e8
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2022-11-25 16:12:57 +0800

    Dataset: IBSR18

--- ABrain/dataset/IBSR.py
+++ ABrain/dataset/IBSR.py
@@ -5,34 +5,33 @@ from torch.utils.data import Dataset
 
 
 class IBSR18(Dataset):
-    def __init__(self,database,target:int=2,transforms:Optional[tio.Transform]=None) -> None:
+    def __init__(self, database, target: int = 2, transforms: Optional[tio.Transform] = None) -> None:
         super().__init__()
-        seg_mode = ["TRI_fill","TRI",""]
+        seg_mode = ["TRI_fill", "TRI", ""]
         self.sids = os.listdir(database)
-        self.database=database
+        self.database = database
         self.mode = seg_mode[target]
-        self.preprocess=tio.transforms.ToCanonical()
-        self.transforms=transforms
-
+        self.preprocess = tio.transforms.ToCanonical()
+        self.transforms = transforms
 
     def __getitem__(self, index):
 
         sid = self.sids[index]
-        img_name = "%s_ana.nii.gz"%sid
-        seg_name = "%s_seg%s_ana.nii.gz"%(sid,self.mode)
-        img = os.path.join(self.database,sid,img_name)
-        seg = os.path.join(self.database,sid,seg_name)
+        img_name = "%s_ana.nii.gz" % sid
+        seg_name = "%s_seg%s_ana.nii.gz" % (sid, self.mode)
+        img = os.path.join(self.database, sid, img_name)
+        seg = os.path.join(self.database, sid, seg_name)
 
         subject = tio.Subject(
-            name = sid,
-            img = self.preprocess(tio.ScalarImage(img)),
-            seg = self.preprocess(tio.LabelMap(seg))
+            name=sid,
+            img=self.preprocess(tio.ScalarImage(img)),
+            seg=self.preprocess(tio.LabelMap(seg))
         )
 
         if self.transforms is not None:
             subject = self.transforms(subject)
 
-        return subject        
+        return subject
 
     def __len__(self):
         return len(self.sids)
--- ABrain/tests/datasets/ibsr.py
+++ ABrain/tests/datasets/ibsr.py
@@ -6,19 +6,20 @@ import torchio as tio
 
 from ...dataset.IBSR import IBSR18
 
+
 class TestIBSR(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.database="/dev/shm/tinyset"
+        self.database = "/dev/shm/tinyset"
 
     def test_ibsr18(self):
         B = 2
         for mode_id in range(3):
-            dataset= IBSR18(os.path.join(self.database,"IBSR18"),mode_id)
-            for x in data.DataLoader(dataset,batch_size=B,shuffle=True):
+            dataset = IBSR18(os.path.join(self.database, "IBSR18"), mode_id)
+            for x in data.DataLoader(dataset, batch_size=B, shuffle=True):
                 names = x["name"]
                 imgs = x["img"][tio.DATA]
                 segs = x["seg"][tio.DATA]
-                self.assertEqual(len(names),B)
-                self.assertEqual(imgs.shape,(B,1,256,128,256))
-                self.assertEqual(segs.shape,(B,1,256,128,256))
+                self.assertEqual(len(names), B)
+                self.assertEqual(imgs.shape, (B, 1, 256, 128, 256))
+                self.assertEqual(segs.shape, (B, 1, 256, 128, 256))
