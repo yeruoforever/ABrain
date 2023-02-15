5954bf733a398a0632e1b1863f31a8ec213f1fe4
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2023-02-15 17:05:37 +0800

    dataset: new IBSR reading

--- ABrain/dataset/IBSR.py
+++ ABrain/dataset/IBSR.py
@@ -3,35 +3,22 @@ from typing import Optional
 import torchio as tio
 from torch.utils.data import Dataset
 
+from .base import OurDataset
 
-class IBSR18(Dataset):
-    def __init__(self, database, target: int = 2, transforms: Optional[tio.Transform] = None) -> None:
-        super().__init__()
+class IBSR18(OurDataset):
+    def __init__(self, database, transforms: Optional[tio.Transform] = None,target:int = 2) -> None:
+        super().__init__(database, True)
+        self.ts=transforms
         seg_mode = ["TRI_fill", "TRI", ""]
-        self.sids = os.listdir(database)
-        self.database = database
         self.mode = seg_mode[target]
-        self.preprocess = tio.transforms.ToCanonical()
-        self.transforms = transforms
 
-    def __getitem__(self, index):
+    def get_samples(self, database):
+        return os.listdir(database)
 
-        sid = self.sids[index]
-        img_name = "%s_ana.nii.gz" % sid
-        seg_name = "%s_seg%s_ana.nii.gz" % (sid, self.mode)
-        img = os.path.join(self.database, sid, img_name)
-        seg = os.path.join(self.database, sid, seg_name)
+    def img_file(self, sid):
+        file =  "%s_ana.nii.gz" % sid 
+        return os.path.join(self.database, sid, file)
 
-        subject = tio.Subject(
-            name=sid,
-            img=self.preprocess(tio.ScalarImage(img)),
-            seg=self.preprocess(tio.LabelMap(seg))
-        )
-
-        if self.transforms is not None:
-            subject = self.transforms(subject)
-
-        return subject
-
-    def __len__(self):
-        return len(self.sids)
+    def seg_file(self, sid):
+        file = "%s_seg%s_ana.nii.gz" % (sid, self.mode)
+        return os.path.join(self.database, sid, file)
