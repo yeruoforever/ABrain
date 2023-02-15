6d554e2d7d5d3649460d4fd7342a15c09c3d81ec
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2023-02-15 17:14:12 +0800

    new API

--- ABrain/trainer/base.py
+++ ABrain/trainer/base.py
@@ -7,8 +7,10 @@ from torch.nn import Module
 from torch.optim import Optimizer
 from torch.utils.data import DataLoader, IterDataPipe
 from tqdm import tqdm
+import time
 
-from .watchdog import WatchDog
+from .watchdog import WatchDog, Sniffer
+from .writer import InferenceWriter
 
 
 class Trainer(object):
@@ -16,15 +18,15 @@ class Trainer(object):
 
     def __init__(self,
                  model: Module,
-                 watcher: WatchDog,
+                 sniffer: Sniffer,
                  gpu: torch.device,
                  ) -> None:
         self.model = model.to(gpu)
-        self.watcher = watcher
+        self.watcher = WatchDog(sniffer)
         self.device = gpu
 
-    def reload_model(self, model: Module) -> None:
-        self.model = model.to(self.device)
+    def reload_model(self, state_dict: dict) -> None:
+        self.model.load_state_dict(state_dict)
 
     def parse_data_train(package: dict) -> Tuple:
         """
@@ -58,7 +60,7 @@ class Trainer(object):
         '''
         raise NotImplementedError()
 
-    def inference_batch(data, **kwargs)->Tensor:
+    def inference_batch(data, **kwargs) -> Tensor:
         raise NotImplementedError()
 
     def train(self, dataloader: DataLoader, loss_fun: Callable, optimer: Optimizer, info: Optional[str] = None):
@@ -69,13 +71,12 @@ class Trainer(object):
             data, target = self.parse_data_train(subject)
             with amp.autocast():
                 loss, pred = self.forward_iterate(data, target, loss_fun)
-            optimer.zero_grad()  
+            optimer.zero_grad()
             scaler.scale(loss).backward()
             scaler.step(optimer)
             scaler.update()
             progress.set_description("train loss: %.6f" % loss.item())
-            # TODO record states for each iters.
-            # self.train_callback(pred,target)
+            self.watcher.catch(loss.item(), pred, target, mode="train")
 
     def validate(self, dataloader: DataLoader, loss_fun: Callable, info: Optional[str] = None):
         self.model.eval()
@@ -85,10 +86,9 @@ class Trainer(object):
             with torch.no_grad():
                 loss, pred = self.forward_iterate(data, target, loss_fun)
             progress.set_description("val loss: %.6f" % loss.item())
-        # TODO
-        # self.validate_callback(pred,target)
+            self.watcher.catch(loss.item(), pred, target, mode="validate")
 
-    def inference(self, dataloader: Union[DataLoader, IterDataPipe], writer, **kwargs):
+    def inference(self, dataloader: Union[DataLoader, IterDataPipe], writer:InferenceWriter, **kwargs):
         self.model.eval()
         for subject in tqdm(dataloader):
             data, meta = self.parse_data_inference(subject)
@@ -99,7 +99,22 @@ class Trainer(object):
                 writer.save(meta, pred)
 
     def epoch_step(self):
-        self.watcher.epoch_end()
+        return self.watcher.step()
+
+
+    def save(self,location:str):
+        pass
+
+    def state_dict(self):
+        state={
+            'watcher':self.watcher.state_dict()
+        }
+        return state
+
+    def load_state(self,state):
+        self.watcher.load_state(state['watcher'])
+
+
 
 
 class CascadedTrainer(Trainer):
