dbf855e47f1fa0c2f03a0e403d83b6a1511353a7
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2023-02-15 17:02:28 +0800

    factory: SchedulerFactory(

--- ABrain/factory.py
+++ ABrain/factory.py
@@ -1,6 +1,9 @@
 import os
 import json
 
+import torch.optim as opt
+from torch.optim import lr_scheduler as lrs
+
 from . import modelzoo
 from .modelzoo import AbstractModel
 from . import dataset
@@ -9,65 +12,75 @@ from . import transforms
 from .transforms import Transform
 
 
-class ModelFactory(object):
+class Factory(object):
+    products = {}
+
+    @classmethod
+    def get(cls, name) -> AbstractModel:
+        assert name in cls.products, KeyError(
+            f"`{name}` not find in Factory.")
+        return cls.products[name]
+
+    @classmethod
+    def list(cls):
+        print("Currently supported products are：")
+        for name in cls.products.keys():
+            print('\t', name)
+
+
+class ModelFactory(Factory):
     '''模型工厂'''
-    model_dict = {name: getattr(modelzoo, name) for name in modelzoo.__all__}
-    del model_dict['AbstractModel']
-    del model_dict['ClassifyModel']
-    del model_dict['SegmentationModel']
-    del model_dict['DeepSupervisedClassify']
-    del model_dict['DeepSupervisedSegmentation']
-
-    @staticmethod
-    def create(model_name, *args, **kwargs) -> AbstractModel:
-        '''创建模型'''
-        assert model_name in ModelFactory.model_dict, \
-            (f"`{model_name}` not find in Model Zoo.")
-        initialler = ModelFactory.model_dict[model_name]
-        return initialler(*args, **kwargs)
-
-    @staticmethod
-    def create_from_config(model_name: str, file_name: str) -> AbstractModel:
-        assert os.path.exists(file_name), "`{file_name}` not in file system."
-        config = json.load(file_name)
-        return ModelFactory.create(model_name, **config)
-
-    @staticmethod
-    def list() -> None:
-        print("Currently supported models are：")
-        for name in ModelFactory.model_dict.keys():
-            print('\t', name)
+    products = {name: getattr(modelzoo, name) for name in modelzoo.__all__}
+    del products['AbstractModel']
+    del products['ClassifyModel']
+    del products['SegmentationModel']
+    del products['DeepSupervisedClassify']
+    del products['DeepSupervisedSegmentation']
 
 
-class DataFactory(object):
-    dataset_dict = {name: getattr(dataset, name) for name in dataset.__all__}
-    del dataset_dict['Dataset']
-
-    @staticmethod
-    def get(ds: str, *args, **kwargs) -> Dataset:
-        assert ds in DataFactory.dataset_dict, \
-            (f"`{ds}` don't support.")
-        return DataFactory.dataset_dict[ds](*args, **kwargs)
-
-    @staticmethod
-    def list():
-        print("Currently supported datasets are：")
-        for name in DataFactory.dataset_dict.keys():
-            print('\t', name)
+class DataFactory(Factory):
+    products = {name: getattr(dataset, name) for name in dataset.__all__}
+    del products['Dataset']
 
 
-class TransfromFactory(object):
-    transforms_dict = {
+class TransfromFactory(Factory):
+    products = {
         name: getattr(transforms, name)
         for name in transforms.__all__
     }
-    del transforms_dict['Transform']
-    @staticmethod
-    def get(ts: str, *args, **kwargs):
-        return TransfromFactory.transforms_dict[ts](*args, *kwargs)
+    del products['Transform']
 
-    @staticmethod
-    def list():
-        print("Currently supported transforms are：")
-        for name in TransfromFactory.transforms_dict.keys():
-            print('\t', name)
+
+class OptimFactory(Factory):
+    products = {
+        name: getattr(opt, name) for name in opt.__dict__ if name[0].isupper()
+    }
+    del products['Optimizer']
+
+
+class SchedulerFactory(Factory):
+    products = {
+        'LambdaLR': lrs.LambdaLR,
+        # 'MultiplicativeLR': lrs.MultiplicativeLR,
+        'StepLR': lrs.StepLR,
+        'MultiStepLR': lrs.MultiStepLR,
+        'ConstantLR': lrs.ConstantLR,
+        'LinearLR': lrs.LinearLR,
+        'ExponentialLR': lrs.ExponentialLR,
+        'SequentialLR': lrs.SequentialLR,
+        'CosineAnnealingLR': lrs.CosineAnnealingLR,
+        'ChainedScheduler': lrs.ChainedScheduler,
+        'ReduceLROnPlateau': lrs.ReduceLROnPlateau,
+        'CyclicLR': lrs.CyclicLR,
+        'CosineAnnealingWarmRestarts': lrs.CosineAnnealingWarmRestarts,
+        # 'OneCycleLR': lrs.OneCycleLR
+    }
+
+
+__all__ = [
+    'ModelFactory',
+    'DataFactory',
+    'TransfromFactory',
+    'OptimFactory',
+    'SchedulerFactory'
+]
