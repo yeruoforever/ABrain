dd6509f2637787fa09a84bf5eef69c1a49887fbb
yeruoforever <19868036+yeruoforever@users.noreply.github.com>
2023-02-15 17:11:56 +0800

    test for Transform, Optim, Scheduler

--- ABrain/tests/models/factory.py
+++ ABrain/tests/models/factory.py
@@ -1,12 +1,14 @@
 import unittest
 
-from ...model import ModelFactory
+from ...factory import *
+from ...pipeline.base import optional
 
 
-class TestFactory(unittest.TestCase):
+
+class TestFactoryModel(unittest.TestCase):
     def test_create(self):
         d = {'n_class': 10, 'in_channels': 1, 'hidden': 5, "other": 999}
-        ModelFactory.create("LeNet5", **d)
+        ModelFactory.get("LeNet5")(**d)
 
     def test_list(self):
         ModelFactory.list()
@@ -15,9 +17,40 @@ class TestFactory(unittest.TestCase):
         import torch
         from ...modelzoo.base import ClassifyModel
         x = torch.rand(32, 1, 32, 32)
-        model = ModelFactory.create("LeNet5", 10, 1)
-        self.assertTrue(isinstance(model,ClassifyModel))
+        model = optional(ModelFactory.get("LeNet5"), 10, 1)
+
+        self.assertTrue(isinstance(model, ClassifyModel))
         y = model(x)
-        print(y.shape)
-        self.assertEqual(y.shape,(32,10))
-        self.assertTrue(torch.all(torch.sum(y,dim=1)-1<=1e-4))
+        self.assertEqual(y.shape, (32, 10))
+        self.assertTrue(torch.all(torch.sum(y, dim=1)-1 <= 1e-4))
+
+
+class TestFactoryDataset(unittest.TestCase):
+    def test_get(self):
+        ds = DataFactory.get("IBSR18")
+        print(ds)
+    def test_list(self):
+        DataFactory.list()
+
+class TestFactoryTransform(unittest.TestCase):
+    def test_get(self):
+        ts = TransfromFactory.get('RGBPCA')
+        print(ts)
+
+    def test_list(self):
+        TransfromFactory.list()
+
+class TestFactoryOptim(unittest.TestCase):
+    def test_get(self):
+        opt =OptimFactory.get('SGD')
+        print(opt)
+    def test_list(self):
+        OptimFactory.list()
+
+class TestFactoryScheduler(unittest.TestCase):
+    def test_get(self):
+        sch=SchedulerFactory.get('CosineAnnealingWarmRestarts')
+        print(sch)
+
+    def test_list(self):
+        SchedulerFactory.list()
